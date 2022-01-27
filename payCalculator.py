2b07f85bd2c069cd7889481e498e5b872633fd1a
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-01-26 22:13:58 -0600

    pay

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,18 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    rate = input("Enter Rate: ")
+    hrs = float(hrs)
+    rate = float(rate)
+    if hrs > 40
+        regpay = 40*float(rate)
+    if hrs < 40
+        otpay = (float(hrs)-40) * (float(rate)*1.5)
+    pay = (hrs*pay)+otpay    
+    print ("Pay:", pay)
+
+
     
     # end assignment
 
@@ -10,4 +21,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+# calculatePay ()
