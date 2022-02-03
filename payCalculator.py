c4691bf7d00ef98edb955b01a74a4f648871111a
Susan Sandbothe <susan.sandbothe@vu.com>
2022-02-03 07:53:53 -0600

    newday

--- payCalculator.py
+++ payCalculator.py
@@ -2,16 +2,14 @@
 def calculatePay () :
 # Implement your solution in between the two comment blocks
     print("calculating pay")
-    # This first line is provided for you
+# This first line is provided for you
     hrs = input("Enter Hours:")
     h = float(hrs)
     xx = input("Enter Rate:")
     x = float(xx)
     if h <= 40: 
         print( h * x)
-    elif h > 40:
-        print(40*x + (h-40)*1.5*x)         
-    # end assignment
+# end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
