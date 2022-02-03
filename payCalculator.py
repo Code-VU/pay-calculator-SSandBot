f80e5c95d6c7e76a9d8e2f546b71b58da39d4519
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-02-02 18:40:03 -0600

    pay

--- payCalculator.py
+++ payCalculator.py
@@ -1,21 +1,20 @@
 # from platform import python_branch
-def calculatePay ():
+def calculatePay () :
 # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours: ")
     float(hrs)
-    rts = input("Enter Rate: ")
-    float(rts)
-    h=float(hrs)
-    if h<=40: 
-        pay = h*float(rts)
-           
-    print ("Pay: ",pay)
+    rate = input("Enter Rate: ")
+    float(rate)
+    if float(hrs) <=40: 
+        Pay = float(hrs)*float(rate)
+         
+    print ("Pay: ")
 # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay ()
+# calculatePay 
