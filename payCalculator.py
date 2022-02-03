0b6c1133dcfe45e1460a3b2a5e16b234b66b7eee
Susan Sandbothe <Susan.Sandbothe@vu.com>
2022-02-02 18:47:08 -0600

    9times

--- payCalculator.py
+++ payCalculator.py
@@ -3,18 +3,18 @@ def calculatePay () :
 # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours: ")
-    float(hrs)
-    rate = input("Enter Rate: ")
-    float(rate)
-    if float(hrs) <=40: 
-        Pay = float(hrs)*float(rate)
-         
-    print ("Pay: ")
-# end assignment
+    hrs = input("Enter Hours:")
+    h = float(hrs)
+    xx = input("Enter Rate:")
+    x = float(xx)
+    if h <= 40: 
+        print( h * x)
+    elif h > 40:
+        print(40*x + (h-40)*1.5*x)         
+    # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay 
+# calculatePay ()
